901bc46f6bd7c0601f7c64230484f52eb6b00e3a
Shubham <shm.code1@gmail.com>
2024-06-09 00:03:25 +0530

    file_handling

---
+++ file.js
@@ -0,0 +1,44 @@
+// file handling
+const fs=require("fs");
+// this is synchronous
+fs.writeFileSync("./test.txt","Hello synchronous");
+
+// 
+// fs.writeFile("./test.txt","Hello Asynchronous",(err,result)=>{
+//   if(err){
+//     console.log("Error",err);
+//   }
+//   else console.log(result);
+// });
+//*************************************
+// READING FROM A FILE
+
+// synchronous
+
+// const result=fs.readFileSync("./contacts.txt","utf-8");
+// console.log(result);
+
+/* asynchronous */
+
+// fs.readFile("./contacts.txt","utf-8",(err,result)=>{
+//   if(err){
+//     console.log("Error : ",err);
+//   }
+//   else{
+//     console.log(result);
+//   }
+// })
+
+/* appending to a file   */
+
+fs.appendFileSync("./test.txt",`${Date.now()} Hey there`);
+
+//copying one file to another
+
+// fs.unlinkSync("./copy.txt");  // to delwte a file
+
+// fs.cpSync("./test.t","copy.txt"); 
+
+// console.log(fs.statSync(" ./contacts.txt").isFile());
+
+fs.mkdir("my-docs/a");
