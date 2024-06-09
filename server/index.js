dacc44bdaaf707149634730f6838c2470a28447f
Shubham <shm.code1@gmail.com>
2024-06-10 02:31:10 +0530

    handling_url

--- server/index.js
+++ server/index.js
@@ -1,18 +1,25 @@
 const http = require("http");
 const fs=require("fs");
+const url=require("url");
 const myServer = http.createServer((req,res) => {
+  if(req.url==='/favicon.ico') return res.end();  // ignoring /favicon.ico
   const log=`${Date.now()}: ${req.url} Getting new request\n`;
+  const myurl=url.parse(req.url,true);
+  console.log(myurl);
   fs.appendFile("log.txt",log,(err,data)=>{
-    // res.end("Hello server again");
-    switch(req.url){
+    switch(myurl.pathname){
       case '/':res.end("This is home page");
       break;
-      case '/about':res.end("this is an about page");
+      case '/about':
+        const username=myurl.query.myname
+        res.end(`HI, ${username}`);
       break;
+      case '/search':
+        const search=myurl.query.search_query;
+        res.end("Here is result for : "+search);
       default:res.end("404 not_found");
     }
   });
-  // res.end("Hello from server again");
 })
 
 myServer.listen(8000,()=> console.log("server started"))
